b78d285195ca5290f8beed589faa522c233ce42c
FlaxHaxx <jon.wikman@blitter.se>
2017-03-17 01:11:40 +0100

    whitespace

--- src/validate.js
+++ src/validate.js
@@ -1,72 +1,72 @@
 angular.module('inputTypes')
 
-    .factory('validate', function () {
-        function personnummer(value) {
-            if (value.length != 13) {
-                return false;
-            }
+.factory('validate', function () {
+    function personnummer(value) {
+        if (value.length != 13) {
+            return false;
+        }
 
-            if (!dateOrSamordningsnummer(value)) {
-                return false;
-            }
+        if (!dateOrSamordningsnummer(value)) {
+            return false;
+        }
 
-            return luhnAlgoritm(value);
+        return luhnAlgoritm(value);
+    }
+    
+    function orgnr(value) {
+        if(value.length != 11 && value.length != 13) {
+            return false;
+        }
+
+        if (!value.match(/^(\d{2})?(\d{2})(\d{2})(\d{2})\-(\d{4})$/)) {
+            return false;
+        }
+        if(parseInt(RegExp.$3) < 20) {
+            return personnummer(value.length == 11 ? '20' + value : value);
+        }
+
+        if(RegExp.$1 != '' && RegExp.$1 != 16) {
+            return false;
+        }
+
+        return luhnAlgoritm(value);
+    }
+
+    function dateOrSamordningsnummer(value) {
+        if (!value.match(/^(\d{4})(\d{2})(\d{2})\-(\d{4})$/)) {
+            return false;
+        }
+
+        var date = new Date(RegExp.$1, RegExp.$2 - 1, RegExp.$3 < 61 ? RegExp.$3 : 1);
+        if(RegExp.$1 < 1840 || date.getFullYear() != RegExp.$1 || date.getMonth() != RegExp.$2 - 1 || (date.getDate() != RegExp.$3 && (RegExp.$3 < 61 || RegExp.$3 > 91))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function luhnAlgoritm(value) {
+        value = value.replace('-', '');
+        if(value.length == 12) {
+            value = value.substring(2);
         }
         
-        function orgnr(value) {
-            if(value.length != 11 && value.length != 13) {
-                return false;
+        var sum = 0;
+        for (var i = 0; i < 10; i++) {
+            var digit = parseInt(value.charAt(i));
+            if (i % 2 === 0) {
+                digit *= 2;
             }
-
-            if (!value.match(/^(\d{2})?(\d{2})(\d{2})(\d{2})\-(\d{4})$/)) {
-                return false;
+            if (digit > 9) {
+                digit -= 9;
             }
-            if(parseInt(RegExp.$3) < 20) {
-                return personnummer(value.length == 11 ? '20' + value : value);
-            }
-
-            if(RegExp.$1 != '' && RegExp.$1 != 16) {
-                return false;
-            }
-
-            return luhnAlgoritm(value);
+            sum += digit;
         }
+        return (sum % 10) === 0;
+    }
 
-        function dateOrSamordningsnummer(value) {
-            if (!value.match(/^(\d{4})(\d{2})(\d{2})\-(\d{4})$/)) {
-                return false;
-            }
-
-            var date = new Date(RegExp.$1, RegExp.$2 - 1, RegExp.$3 < 61 ? RegExp.$3 : 1);
-            if(RegExp.$1 < 1840 || date.getFullYear() != RegExp.$1 || date.getMonth() != RegExp.$2 - 1 || (date.getDate() != RegExp.$3 && (RegExp.$3 < 61 || RegExp.$3 > 91))) {
-                return false;
-            }
-
-            return true;
-        }
-
-        function luhnAlgoritm(value) {
-            value = value.replace('-', '');
-            if(value.length == 12) {
-                value = value.substring(2);
-            }
-            
-            var sum = 0;
-            for (var i = 0; i < 10; i++) {
-                var digit = parseInt(value.charAt(i));
-                if (i % 2 === 0) {
-                    digit *= 2;
-                }
-                if (digit > 9) {
-                    digit -= 9;
-                }
-                sum += digit;
-            }
-            return (sum % 10) === 0;
-        }
-
-        return {
-            personnummer: personnummer,
-            orgnr: orgnr
-        }
-    });
+    return {
+        personnummer: personnummer,
+        orgnr: orgnr
+    }
+});
