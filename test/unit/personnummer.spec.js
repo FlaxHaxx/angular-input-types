b538913f05477e75cb867080d743394716e96b27
FlaxHaxx <jon.wikman@blitter.se>
2017-05-15 20:21:49 +0200

    Translated test descriptions from swedish to english

--- test/unit/personnummer.spec.js
+++ test/unit/personnummer.spec.js
@@ -9,30 +9,30 @@ describe('personnummer', function () {
             $rootScope.$digest();
             form = $rootScope.form;
         }));
-        it('ska validera tomt personnummer', function () {
+        it('should validate empty personnummer', function () {
             expect(form.personnummer.$valid).toBe(true);
         });
-        it('ska validera personnummer 20121212-1212', function () {
+        it('should validate personnummer 20121212-1212', function () {
             form.personnummer.$setViewValue('20121212-1212');
             expect(form.personnummer.$valid).toBe(true);
         });
-        it('ska inte validera personnummer 121212-1212', function () {
+        it('should not validate personnummer 121212-1212', function () {
             form.personnummer.$setViewValue('121212-1212');
             expect(form.personnummer.$valid).toBe(false);
         });
-        it('ska inte validera personnummer 201212121212', function () {
+        it('should not validate personnummer 201212121212', function () {
             form.personnummer.$setViewValue('201212121212');
             expect(form.personnummer.$valid).toBe(false);
         });
-        it('ska inte validera organisationsnummer 556100-1123 (aktiebolag)', inject(function (validate) {
+        it('should not validate organisationsnummer 556100-1123 (aktiebolag)', inject(function (validate) {
             form.personnummer.$setViewValue('556100-1123');
             expect(form.personnummer.$valid).toBe(false);
         }));
-        it('ska inte validera organisationsnummer 16556100-1123 (aktiebolag)', inject(function (validate) {
+        it('should not validate organisationsnummer 16556100-1123 (aktiebolag)', inject(function (validate) {
             form.personnummer.$setViewValue('16556100-1123');
             expect(form.personnummer.$valid).toBe(false);
         }));
-        it('ska inte validera personnummer 201212121-1234 (felaktig kontrollsiffra)', function () {
+        it('should not validate personnummer 201212121-1234 (wrong kontrollsiffra)', function () {
             form.personnummer.$setViewValue('201212121-1234');
             expect(form.personnummer.$valid).toBe(false);
         });
