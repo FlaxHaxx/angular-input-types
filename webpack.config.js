08aacfa7890fb7678b66b84312ce4b86cc3098a3
FlaxHaxx <jon.wikman@blitter.se>
2017-05-16 15:10:56 +0200

    Getting name for angular-input-types.min.js from package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const webpack = require('webpack');
 var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const distDir = 'dist';
 
 module.exports = {
   entry: {
     app: './src/_module.js'
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'angular-input-types.min.js'
+    path: path.resolve(__dirname, distDir),
+    filename: require('./package.json').name + '.min.js'
   },
   plugins: [
+    new CleanWebpackPlugin(distDir),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.BannerPlugin({banner: 'Angular Input Types v' + require('./package.json').version + `
 (c) 2017 Jon Wikman. https://github.com/FlaxHaxx/angular-input-types
