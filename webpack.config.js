899d7c33e10115c5620e0edeca9cd4191c863de1
FlaxHaxx <jon.wikman@blitter.se>
2017-05-16 15:30:04 +0200

    Removing clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
 const distDir = 'dist';
 
 module.exports = {
@@ -13,7 +11,6 @@ module.exports = {
     filename: require('./package.json').name + '.min.js'
   },
   plugins: [
-    new CleanWebpackPlugin(distDir),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.BannerPlugin({banner: 'Angular Input Types v' + require('./package.json').version + `
 (c) 2017 Jon Wikman. https://github.com/FlaxHaxx/angular-input-types
