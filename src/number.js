c469f09f8b590ae22e97bedfbfc4f7c8ea5115db
FlaxHaxx <jon.wikman@blitter.se>
2017-06-05 15:55:52 +0200

    cursor pos

--- src/number.js
+++ src/number.js
@@ -32,7 +32,6 @@ angular.module('inputTypes')
     }
 
     function setViewValue(inputElement, plainNumberValue, scope) {
-        inputElement.triggerHandler('input');
         var cursorPosition = inputUtils.getCursorPos(inputElement[0]);
         inputElement.val(plainNumberValue === null ? '' : format(plainNumberValue));
         if(plainNumberValue !== null) {
@@ -74,6 +73,7 @@ angular.module('inputTypes')
                 var modelValue = $parse(attrs.ngModel)(scope);
                 if(modelValue) {
                     setViewValue(elm, plainNumber(modelValue), scope);
+                    elm.triggerHandler('change');
                 }
             }
         }
